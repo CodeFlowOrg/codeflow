ea2edd0be74386467f9b2a6c9b5d6e438ceab7b4
hkaur008 <hargunalone@gmail.com>
2020-03-19 12:43:24 +0530

    Updates

---
+++ precache-manifest.79516a61ee0938ad47bb5c0c1b67bdc0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8aef44b0ddba30177717f9a7d064864",
+    "url": "/shiksha-work/index.html"
+  },
+  {
+    "revision": "3259093a97756a8f52a4",
+    "url": "/shiksha-work/static/css/main.30ab4cbb.chunk.css"
+  },
+  {
+    "revision": "9158554045320e62dd9d",
+    "url": "/shiksha-work/static/js/2.7a1c1a35.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/shiksha-work/static/js/2.7a1c1a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3259093a97756a8f52a4",
+    "url": "/shiksha-work/static/js/main.ccc1bf16.chunk.js"
+  },
+  {
+    "revision": "dc0d8cddaf145051b23e",
+    "url": "/shiksha-work/static/js/runtime-main.17cd0467.js"
+  },
+  {
+    "revision": "52063952976fb3458fa83a8181f0ca7a",
+    "url": "/shiksha-work/static/media/image 2.52063952.jpg"
+  },
+  {
+    "revision": "8a248c5d45a267c0e78a916d73bedcce",
+    "url": "/shiksha-work/static/media/image 3.8a248c5d.jpg"
+  },
+  {
+    "revision": "42eeebd4a322915acd3e983ae16b2bdb",
+    "url": "/shiksha-work/static/media/image 4.42eeebd4.jpg"
+  },
+  {
+    "revision": "415b6062f1cbcbdd1af3e625a304e69b",
+    "url": "/shiksha-work/static/media/image 5.415b6062.jpg"
+  },
+  {
+    "revision": "cd1a6d8209ce20e2c01b777a4bd07f70",
+    "url": "/shiksha-work/static/media/image 6.cd1a6d82.jpeg"
+  }
+]);
