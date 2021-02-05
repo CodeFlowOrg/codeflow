abeedfac796e25d946c27954f3b6fca6f32c24ef
hkaur008 <hargunalone@gmail.com>
2021-02-05 17:58:09 +0530

    Updates

---
+++ precache-manifest.3359124722f0ce50bbea4235bb76e626.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68e97deb5cd8a9529b095fcbb406148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52e7eebbcd42c4c42aa",
+    "url": "/static/css/main.6bdf080d.chunk.css"
+  },
+  {
+    "revision": "ffef7e4afc6b02433455",
+    "url": "/static/js/2.fbcb5763.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.fbcb5763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52e7eebbcd42c4c42aa",
+    "url": "/static/js/main.f1473c8a.chunk.js"
+  },
+  {
+    "revision": "f77706b69c0058ef3f84",
+    "url": "/static/js/runtime-main.cfe16cbd.js"
+  },
+  {
+    "revision": "c83ae83b98bfa5e7684249118c6e8283",
+    "url": "/static/media/Tiny cute children learning coding.c83ae83b.png"
+  },
+  {
+    "revision": "d740b8424b3566073c7975c773a4250d",
+    "url": "/static/media/ex-header-background.d740b842.jpg"
+  },
+  {
+    "revision": "b41ed9a7de904427a3af75362ca137f3",
+    "url": "/static/media/logo.b41ed9a7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codeflow/precache-manifest.5583bfdf89e702722dc3b02369949da2.js"
+  "/precache-manifest.3359124722f0ce50bbea4235bb76e626.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codeflow/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
