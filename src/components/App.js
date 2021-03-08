8e733bb8bc22163c0d54ee4e92f46654c60ffeb1
hkaur008 <hargunalone@gmail.com>
2021-03-09 03:30:05 +0530

    public url env changes

--- src/components/App.js
+++ src/components/App.js
@@ -14,22 +14,48 @@ function App() {
         <Navbar bg="primary" variant="dark">
           <Navbar.Brand href="/">Codeflow</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/events">Events</Nav.Link>
-            <Nav.Link href="/teams">Team</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL + "/"}>Home</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL + "/about"}>About</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL + "/event"}>Events</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL + "/teams"}>Team</Nav.Link>
+            <Nav.Link href={process.env.PUBLIC_URL + "/contact"}>
+              Contact Us
+            </Nav.Link>
           </Nav>
         </Navbar>
 
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/team" exact={true} component={Team} />
-        <Route path="/events" exact={true} component={Events} />
+        <Route
+          path="{process.env.PUBLIC_URL + '/'}"
+          exact={true}
+          component={Home}
+        />
+        <Route
+          path={process.env.PUBLIC_URL + "/teams"}
+          exact={true}
+          component={Team}
+        />
+        <Route
+          path={process.env.PUBLIC_URL + "/event"}
+          exact={true}
+          component={Events}
+        />
 
-        <Route path="/about" exact={true} component={About} />
-        <Route path="/contact" exact={true} component={Contact} />
+        <Route
+          path={process.env.PUBLIC_URL + "/about"}
+          exact={true}
+          component={About}
+        />
+        <Route
+          path={process.env.PUBLIC_URL + "/contact"}
+          exact={true}
+          component={Contact}
+        />
 
-        <Route path="/Add" exact={true} component={Addevent} />
+        <Route
+          path={process.env.PUBLIC_URL + "/add"}
+          exact={true}
+          component={Addevent}
+        />
       </div>
     </Router>
   );
