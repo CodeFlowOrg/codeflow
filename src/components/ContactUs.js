39191ee76e9c8bba487c63b68fcf5ca24c168978
Nirpendra Chaudhary <nirpendra09@gmail.com>
2021-04-02 07:26:30 +0000

    "Form implemented"

---
+++ src/components/ContactUs.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Grow, Container, TextField, Button, Typography, Paper } from '@material-ui/core';
+
+import useStyles from './ContactStyle'
+
+
+
+const ContactUs = () => {
+    const classes = useStyles();
+
+    const [postData, setPostData] = useState({ name: '', email: '', message: '' });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPostData(e.target.value);
+        console.log(postData);
+    }
+    return (
+        <Container className={ classes.container }>
+            <Grow in>
+
+
+                <Paper className={ classes.paper } elevation={ 3 } >
+                    <form autoComplete="off" noValidate className={ `${classes.root} ${classes.form}` }>
+                        <Typography variant="h6">Contact Us
+            </Typography>
+
+                        <TextField name="name" variant="outlined" label="Name" fullWidth onChange={ (e) => setPostData({ ...postData, name: e.target.value }) } />
+
+                        <TextField name="email" variant="outlined" label="Email" fullWidth onChange={ (e) => setPostData({ ...postData, email: e.target.value }) } />
+
+                        <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={ 4 } onChange={ (e) => setPostData({ ...postData, message: e.target.value }) } />
+
+
+
+                        <Button classes={ { contained: classes.btnCol, label: classes.label } } variant="contained" size="large" type="submit" onClick={ handleSubmit } fullWidth>Submit</Button>
+
+                        <Button variant="contained" color="secondary" size="small" fullWidth>Clear</Button>
+                    </form>
+                </Paper >
+
+            </Grow>
+        </Container>
+    )
+}
+
+export default ContactUs
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,90 +1,92 @@
 import "./styles/contact.css";
 import Customnav from "./customnavbar";
 import {
-  FaLinkedin,
-  FaGithubSquare,
-  FaTwitterSquare,
-  FaYoutube,
-  FaTelegram,
+    FaLinkedin,
+    FaGithubSquare,
+    FaTwitterSquare,
+    FaYoutube,
+    FaTelegram,
 } from "react-icons/fa";
 import { Card } from "react-bootstrap";
+import ContactUs from './ContactUs'
 
 function Contact() {
-  return (
-    <div className="contact">
-      <Customnav color="#01bfd9" height="50px" padding="5vh" />
-      <div className="nhi-custom">
-        <a href="https://github.com/codeflow201" target="_blank">
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Text>
-                <div className="m-1">
-                  <FaGithubSquare className="nav-dynamic-button" size={40} />{" "}
+    return (
+        <div>
+            <Customnav color="#01bfd9" height="50px" padding="5vh" />
+            <ContactUs />
+            <div className="nhi-custom">
+                <a href="https://github.com/codeflow201" target="_blank">
+                    <Card className="text-center">
+                        <Card.Body>
+                            <Card.Text>
+                                <div className="m-1">
+                                    <FaGithubSquare className="nav-dynamic-button" size={ 40 } />{ " " }
                   Github
                 </div>
                 Check out our GitHub repository to see our project boards, code
                 and spike reports.
               </Card.Text>
-            </Card.Body>
-          </Card>
-        </a>
-        <a
-          href="https://www.youtube.com/channel/UCIsECRBRWOalTC51co-tJWg"
-          target="_blank"
-        >
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Text>
-                <div className="m-1">
-                  <FaYoutube className="nav-dynamic-button" size={40} /> Youtube
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a
+                    href="https://www.youtube.com/channel/UCIsECRBRWOalTC51co-tJWg"
+                    target="_blank"
+                >
+                    <Card className="text-center">
+                        <Card.Body>
+                            <Card.Text>
+                                <div className="m-1">
+                                    <FaYoutube className="nav-dynamic-button" size={ 40 } /> Youtube
                 </div>
                 Check out our videos.
               </Card.Text>
-            </Card.Body>
-          </Card>
-        </a>
-        <a href="https://www.linkedin.com/company/codefloworg/" target="_blank">
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Text>
-                <div className="m-1">
-                  <FaLinkedin className="nav-dynamic-button" size={40} /> Linked
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a href="https://www.linkedin.com/company/codefloworg/" target="_blank">
+                    <Card className="text-center">
+                        <Card.Body>
+                            <Card.Text>
+                                <div className="m-1">
+                                    <FaLinkedin className="nav-dynamic-button" size={ 40 } /> Linked
                   in
                 </div>
                 Check out , our linked in.!!
               </Card.Text>
-            </Card.Body>
-          </Card>
-        </a>
-        <a href="https://twitter.com/codefloworg" target="_blank">
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Text>
-                <div className="m-1">
-                  <FaTwitterSquare className="nav-dynamic-button" size={40} />{" "}
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a href="https://twitter.com/codefloworg" target="_blank">
+                    <Card className="text-center">
+                        <Card.Body>
+                            <Card.Text>
+                                <div className="m-1">
+                                    <FaTwitterSquare className="nav-dynamic-button" size={ 40 } />{ " " }
                   Twitter
                 </div>
                 See our latest tweets .!!
               </Card.Text>
-            </Card.Body>
-          </Card>
-        </a>
-        <a href="https://t.me/joinchat/FdJhyMumJK5sryCv" target="_blank">
-          <Card className="text-center">
-            <Card.Body>
-              <Card.Text>
-                <div className="m-1">
-                  <FaTelegram className="nav-dynamic-button" size={40} />{" "}
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a href="https://t.me/joinchat/FdJhyMumJK5sryCv" target="_blank">
+                    <Card className="text-center">
+                        <Card.Body>
+                            <Card.Text>
+                                <div className="m-1">
+                                    <FaTelegram className="nav-dynamic-button" size={ 40 } />{ " " }
                   Telegram
                 </div>
                 Join us on Telegram !!
               </Card.Text>
-            </Card.Body>
-          </Card>
-        </a>
-      </div>
-    </div>
-  );
+                        </Card.Body>
+                    </Card>
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default Contact;
