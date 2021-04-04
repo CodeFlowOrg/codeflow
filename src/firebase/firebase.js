926b3d9700d865e004c14ca7ad0e3afab0371591
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-04 18:21:53 +0530

    Added .env file values here

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const Config = {
-  apiKey: "AIzaSyAc1HwKpWdsEPUBaU0y9mWmla5EU3_vqCQ",
-  authDomain: "codeflow-org.firebaseapp.com",
-  databaseURL: "https://codeflow-org-default-rtdb.firebaseio.com",
-  projectId: "codeflow-org",
-  storageBucket: "codeflow-org.appspot.com",
-  messagingSenderId: "706543120697",
-  appId: "1:706543120697:web:e90a5abaa092a306c778e8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_Id,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(Config);
