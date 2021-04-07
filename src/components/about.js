5789ad1ef4a152d7b6ac740a143c3454aeae3a8e
Paras Bansal <67871592+iamparasbansal@users.noreply.github.com>
2021-04-07 11:35:26 +0530

    Revert "'Home fixed'"

--- src/components/about.js
+++ src/components/about.js
@@ -7,45 +7,45 @@ import youtube from "../assets/youtube.png";
 import opensource from "../assets/opensource.jpg";
 
 function About() {
-    return (
-        <div>
-            <Customnav color="#01bfd9" height="50px" />
-            <div>
-                { " " }
-                <h6 className="Collabrated">Collaborated With</h6>
-            </div>
-            <div className=" justify-content-center">
-                <div className="">
-                    {/* <img className="img-fluid" src={colla} alt="Image" /> */ }
-                </div>
-            </div>
-
-            <div className="focus">Our Focus Areas</div>
-            <div className="container">
-                <div className="row about__cards">
-                    <div className="about__card ">
-                        <img className="img1" src={ team } alt="Image" />
-
-                        <div>
-                            <span className="caption orange ">Community Building</span>
-                        </div>
-                    </div>
-                    <div className="about__card ">
-                        <img className="img1 img-opensource" src={ opensource } alt="Image" />
-                        <div>
-                            <span className="caption green">Open Source</span>
-                        </div>
-                    </div>
-                    <div className="about__card ">
-                        <img className="img1 img-youtube" src={ youtube } alt="Image" />
-                        <div>
-                            <span className="caption orange">Youtube</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <Customnav color="#01bfd9" height="50px" />
+      <div>
+        {" "}
+        <h6 className="Collabrated">Collaborated With</h6>
+      </div>
+      <div className=" justify-content-center">
+        <div className="">
+          {/* <img className="img-fluid" src={colla} alt="Image" /> */}
         </div>
-    );
+      </div>
+
+      <div className="focus">Our Focus Areas</div>
+      <div className="container">
+        <div className="row about__cards">
+          <div className="about__card ">
+            <img className="img1" src={team} alt="Image" />
+
+            <div>
+              <span className="caption orange ">Community Building</span>
+            </div>
+          </div>
+          <div className="about__card ">
+            <img className="img1 img-opensource" src={opensource} alt="Image" />
+            <div>
+              <span className="caption green">Open Source</span>
+            </div>
+          </div>
+          <div className="about__card ">
+            <img className="img1 img-youtube" src={youtube} alt="Image" />
+            <div>
+              <span className="caption orange">Youtube</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
