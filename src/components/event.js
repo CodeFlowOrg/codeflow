a9454522967054d1de950a3cfb5a4824cf27573a
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-08 10:25:49 +0530

    up comming event div styled little bit

--- src/components/event.js
+++ src/components/event.js
@@ -1,4 +1,4 @@
- import useFirestore from "./hooks/useFirestore";
+import useFirestore from "./hooks/useFirestore";
 import "./styles/App.css";
 import { useState } from "react";
 import { Card, Accordion, Button } from "react-bootstrap";
@@ -12,45 +12,55 @@ const Events = () => {
     setIndex(selectedIndex);
   };
   return (
-    <div className="events ">
-      <Customnav color="#01bfd9" height="50px" padding="5vh" />
-      <div class="focus">Events</div>
-      <div className=" ">
-        <div className="row justify-content-center row-custom">
-          {docs &&
-            docs.map((doc) => (
-              <div className="col-md-6 col-lg-3  col-sm-12 margin-card event-div">
-                <Accordion defaultActiveKey="0">
-                  <Card>
-                  <div className="card-banner">
-                    <img
-                      src={doc.url}
-                      className="img.fluid
+    <>
+      <div className="events ">
+        <Customnav color="#01bfd9" height="50px" padding="5vh" />
+        <div id="up-coming-events">
+          <div id="event-thumbnail"></div>
+          <p>hello upComming eventts here</p>
+        </div>
+        {/* <div class="focus">Events</div> */}
+        <div className=" ">
+          <div className="row justify-content-center row-custom">
+            {docs &&
+              docs.map((doc) => (
+                <div className="col-md-6 col-lg-3  col-sm-12 margin-card event-div">
+                  <Accordion defaultActiveKey="0">
+                    <Card>
+                      <div className="card-banner">
+                        <img
+                          src={doc.url}
+                          className="img.fluid
                    "
-                    />
+                        />
 
-                    <div className="text-white bg-warning p-2 title-div">
-                      {" "}
-                      {doc.title}
-                    </div>
-                    <Card.Header>
-                      <Accordion.Toggle as={Button} variant="info" eventKey="1" >
-                        Know More !
-                      </Accordion.Toggle>
-                    </Card.Header>
-                    </div>
-                    <Accordion.Collapse eventKey="1">
-                      <Card.Body>
-                        <p>{doc.content}</p>
-                      </Card.Body>
-                    </Accordion.Collapse>
-                  </Card>
-                </Accordion>
-              </div>
-            ))}
+                        <div className="text-white bg-warning p-2 title-div">
+                          {" "}
+                          {doc.title}
+                        </div>
+                        <Card.Header>
+                          <Accordion.Toggle
+                            as={Button}
+                            variant="info"
+                            eventKey="1"
+                          >
+                            Know More !
+                          </Accordion.Toggle>
+                        </Card.Header>
+                      </div>
+                      <Accordion.Collapse eventKey="1">
+                        <Card.Body>
+                          <p>{doc.content}</p>
+                        </Card.Body>
+                      </Accordion.Collapse>
+                    </Card>
+                  </Accordion>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
