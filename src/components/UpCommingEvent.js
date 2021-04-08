ca2130f4abceb6aac7e85dc477ab76815fa9a30f
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-08 18:02:48 +0530

    added text

--- src/components/UpCommingEvent.js
+++ src/components/UpCommingEvent.js
@@ -1,10 +1,17 @@
 import React from "react";
-import "./styles/upcommingevent.css"
+import "./styles/upcommingevent.css";
 function UpCommingEvent() {
   return (
     <div id="up-coming-events">
-      <div id="event-thumbnail"></div>
-      <p>hello upComming eventts here</p>
+      <div id="event-thumbnail">
+        <p>(Thumbnail)</p>
+      </div>
+      <div id="event-details">
+        <h2>Heading Of The Event</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore magnam ea iure nam, dolor voluptas reiciendis enim, quia, aliquid doloremque officia aut.
+        </p>
+      </div>
     </div>
   );
 }
