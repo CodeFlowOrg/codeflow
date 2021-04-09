b69c374f98fa7007314624d0cd1757ef0a56a4e3
Paras Bansal <67871592+iamparasbansal@users.noreply.github.com>
2021-04-09 21:56:00 +0530

    Revert "Build complete home page except testimonials section as directed in the issue and made it responsive for all screen sizes"

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,10 @@
 import "./styles/Home.css";
 import img1 from "../assets/img1.png";
 import Customnav from "./customnavbar";
-import team from "../assets/team.jpg";
-import youtube from "../assets/youtube.png";
-import opensource from "../assets/opensource.jpg";
-import TeamCard from "./TeamCard";
 
 function Home() {
   return (
-    <>
     <div className="Home" id="Home">
-      <Customnav color="" height="50px" padding="5vh"/>  
-    <div className="parent">
-      <div class="col-5 son"> 
-      <div className="son__Para">
-          <h3>
-            <span>Codeflow</span>
-          </h3>
-          <h3> Learning must go on!</h3>
-          <p>Codeflow is an organisatiom whose main motive is to empower student community by organising webinars, hackathons and open source events</p>
       <Customnav color="" height="50px" padding="5vh" />
       <div className="parent">
         <div class="col-5 son">
@@ -31,126 +17,9 @@ function Home() {
         </div>
         <div class="col-7 daughter">
           <img src={img1} alt="loading" />
-      </div>
-      </div>
-    </div>
-    </div>
-    <div className="home__about">
-      <div className="home__aboutInfo">
-        <h3><span>About</span> Us</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, neque sed! Quas quisquam laudantium aspernatur laboriosam sint, quod blanditiis? Assumenda veritatis consequuntur obcaecati blanditiis? Earum cum tenetur dolorem. Possimus, similique.Animi, neque sed! Quas quisquam laudantium aspernatur laboriosam sint, quod blanditiis? Assumenda veritatis consequuntur obcaecati blanditiis? Earum cum tenetur dolorem. Possimus, similique.
-        </p>
-      </div>
-      <div className="home__aboutCards">
-        <div className="home__aboutCard1">
-          <img src={opensource} alt="thumbnail" />
-          <h5>OpenSource</h5>
-        </div>
-        <div className="home__aboutCard1">
-          <img src={team} alt="thumbnail" />
-          <h5>Community building</h5>
-        </div>
-        <div className="home__aboutCard1">
-          <img src={youtube} alt="thumbnail" />
-          <h5>Events and Workshops</h5>
         </div>
       </div>
     </div>
-    <div className="home__team">
-      <h3><span>Core</span> Team</h3>
-      <div className="home__teamCards">
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     position='(Unknown)'
-     />
-      </div>
-      <div className="home__teamCardsContributor">
-        <h3><span>Top</span> Contributor</h3>
-        <div className="home__teamCards">
-        <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-     <TeamCard
-     image={youtube} 
-     name="Abra ka dabra"
-     />
-      </div>
-      </div>
-    </div>
-    </>
   );
 }
 
