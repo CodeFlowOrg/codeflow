a3e22d3e0003de1305f5ce3420d0746463b1a8fe
Rohan Modi <rohanmodi790@gmail.com>
2021-10-06 23:33:16 +0530

    Contact Page UI changed Successfully

--- src/components/pages/openSource.js
+++ src/components/pages/openSource.js
@@ -1,109 +1,111 @@
 import "../styles/contact.css";
 import Customnav from "../layout/customnavbar";
+import head from "../../assets/head.svg";
+import { SocialIcon } from 'react-social-icons';
+
 import {
-  FaLinkedin,
-  FaGithubSquare,
-  FaTwitterSquare,
-  FaYoutube,
-  FaTelegram,
+    FaLinkedin,
+    FaGithubSquare,
+    FaTwitterSquare,
+    FaYoutube,
+    FaTelegram,
 } from "react-icons/fa";
 import useForm from "../hooks/useForm";
 import validator from "../utilities/validator";
 // import { Card } from "react-bootstrap";
 
 function Collaboration() {
-  return (
-    <div className="contact">
-      {/* <Customnav color="#01bfd9" height="50px" padding="5vh" /> */}
-      <div className="contact__Packet">
-        <h1> Contact Us </h1>{" "}
-        <div className="contact__container">
-          <div className="contactContainer__left">
-            <div className="contactContainer__left-info">
-              <h4> Contact Information </h4>{" "}
-              <p>
-                {" "}
-                Lorem ipsum dolor sit amet consectetur adipisicing
-                elit.Aspernatur magnam quis autem inventore laboriosam dicta
-                dolorem error ea, suscipit nobis ut, similique velit vitae
-                explicabo.{" "}
-              </p>{" "}
-            </div>{" "}
-            <div className="contactContainer__leftIcons">
-              <a
-                href="https://github.com/codeflow201"
-                target="_blank"
-                rel="noreferrer"
-                className="contactContainer__leftIcon"
-              >
-                <FaGithubSquare className="contact__icon" size={23.5} />{" "}
-              </a>{" "}
-              <a
-                href="https://www.linkedin.com/company/codefloworg/"
-                target="_blank"
-                rel="noreferrer"
-                className="contactContainer__leftIcon"
-              >
-                <FaLinkedin className="contact__icon" size={23.5} />{" "}
-              </a>{" "}
-              <a
-                href="https://twitter.com/codefloworg"
-                target="_blank"
-                rel="noreferrer"
-                className="contactContainer__leftIcon"
-              >
-                <FaTwitterSquare className="contact__icon" size={23.5} />{" "}
-              </a>{" "}
-              <a
-                href="https://www.youtube.com/channel/UCIsECRBRWOalTC51co-tJWg"
-                target="_blank"
-                rel="noreferrer"
-                className="contactContainer__leftIcon"
-              >
-                <FaYoutube className="contact__icon" size={23.5} />{" "}
-              </a>{" "}
-              <a
-                href="https://t.me/joinchat/FdJhyMumJK5sryCv"
-                target="_blank"
-                rel="noreferrer"
-                className="contactContainer__leftIcon"
-              >
-                <FaTelegram className="contact__icon" size={23.5} />{" "}
-              </a>{" "}
-            </div>{" "}
-          </div>{" "}
-          <form action="https://formcarry.com/s/S2thQbCqEvW" method="POST">
-            <div className="form__first">
-              <div className="form__first-group">
-                <label> First Name </label>{" "}
-                <input name="firstname" type="text" />
-              </div>{" "}
-              <div className="form__first-group">
-                <label> Last Name </label> <input name="lastname" type="text" />
-              </div>{" "}
-            </div>{" "}
-            <div className="form__first">
-              <div className="form__first-group">
-                <label> E - Mail </label> <input name="email" type="email" />
-              </div>{" "}
-              <div className="form__first-group">
-                <label> Phone - No </label> <input name="phone" type="tel" />
-              </div>{" "}
-            </div>{" "}
-            <div className="form__first">
-              <div className="form__first-group">
-                <label> Message </label> <textarea name="message"> </textarea>{" "}
-              </div>{" "}
-            </div>{" "}
-            <button type="submit" className="form__btn">
-              {" "}
-              Submit{" "}
-            </button>{" "}
-          </form>{" "}
-        </div>{" "}
-      </div>{" "}
-    </div>
-  );
+    return ( <
+        div className = "contact" > { /* <Customnav color="#01bfd9" height="50px" padding="5vh" /> */ } <
+        div className = "contact__Packet" >
+        <
+        h1 > Contact Us < /h1>{" "} <
+        div className = "contact__container" >
+        <
+        div className = "contactContainer__left" >
+        <
+        div className = "contactContainer__left-info" >
+        <
+        h3 > Any queries, feel free to Contact Us...! < /h3>{" "} <
+        p > { " " } <
+        img height = { 250 }
+        src = { head }
+        alt = "header" / > { " " } <
+        /p>{" "} < /
+        div > { " " } <
+        div className = "contactContainer__leftIcons" >
+        <
+        SocialIcon url = "https://twitter.com/codefloworg"
+        fgColor = "#FFFFFF" / >
+        <
+        SocialIcon className = "id"
+        url = "https://www.youtube.com/channel/UCIsECRBRWOalTC51co-tJWg"
+        fgColor = "#FFFFFF" / >
+        <
+        SocialIcon className = "id1"
+        url = "https://www.linkedin.com/company/codefloworg/"
+        fgColor = "#FFFFFF" / >
+        <
+        SocialIcon className = "id2"
+        url = "https://github.com/codeflow201"
+        fgColor = "#FFFFFF" /
+        >
+        <
+        SocialIcon className = "id3"
+        url = "https://t.me/joinchat/FdJhyMumJK5sryCv"
+        fgColor = "#FFFFFF"
+        network = "telegram" /
+        >
+        <
+        /div> { " " } < /
+        div > { " " } <
+        form action = "https://formcarry.com/s/S2thQbCqEvW"
+        method = "POST" >
+        <
+        div className = "form__first" >
+        <
+        div className = "form__first-group" >
+        <
+        label > First Name < /label>{" "} <
+        input name = "firstname"
+        type = "text" / >
+        <
+        /div>{" "} <
+        div className = "form__first-group" >
+        <
+        label > Last Name < /label> <input name="lastname" type="text" / >
+        <
+        /div>{" "} < /
+        div > { " " } <
+        div className = "form__first" >
+        <
+        div className = "form__first-group" >
+        <
+        label > E - Mail < /label> <input name="email" type="email" / >
+        <
+        /div>{" "} <
+        div className = "form__first-group" >
+        <
+        label > Phone - No < /label> <input name="phone" type="tel" / >
+        <
+        /div>{" "} < /
+        div > { " " } <
+        div className = "form__first" >
+        <
+        div className = "form__first-group" >
+        <
+        label > Message < /label> <textarea name="message"> </textarea > { " " } <
+        /div>{" "} < /
+        div > { " " } <
+        button type = "submit"
+        className = "form__btn" > { " " }
+        Submit { " " } <
+        /button>{" "} < /
+        form > { " " } <
+        /div>{" "} < /
+        div > { " " } <
+        /div>
+    );
 }
 
 export default Collaboration;
