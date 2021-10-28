ca3bfad88ed71c4bb7192e0d686080bb8c5a2651
Tushar Sachan <62793231+CyberGhost2023@users.noreply.github.com>
2021-10-28 23:58:11 +0530

    Update Footer.js
    Added new footer with social media icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,69 +1,51 @@
-import logo from "../../assets/logo.png";
-import img from "../../assets/Qr-code.png";
 import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-styling">
-      <footer>
-        <div className="container">
-          <div className="footer-logo">
-            <Link to="/">
-              <img src={logo} alt="Codeflow logo" width="100" />
-            </Link>
-            <div className="footer-logo-text">
-              <h2>Codeflow</h2>
-              <p>Learning must go on!</p>
-            </div>
-          </div>
-          <div className="footer-link-group">
-          <h3>Quick Links</h3>
-            <Link to="/">Home</Link>
-            <Link to="/events">Events</Link>
-            <Link to="/hackathon">Hackathons</Link>
-            <Link to="/">Open Source</Link>
-            <Link to="/">Collaborations</Link>
-          </div>
-          <div className="footer-link-group social-media-links">
-            <h3>Social Media Links</h3>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer">
-              Facebook
-            </a>
-            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-              Twitter
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer">
-              Instagram
-            </a>
-          </div>
-          <div className="footer-link-group other-links">
-            <h3>Other Links</h3>
-            <a href="https://www.github.com/codeflow201" target="_blank" rel="noreferrer">
-              Github
-            </a>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noreferrer">
-              LinkedIn
-            </a>
-            <a href="mailto:codefloworg@gmail.com" target="_blank" rel="noreferrer">
-              Email Us
-            </a>
-          </div>
-          <div>
-          <img src={img} width="200px" height="200px" />
-          </div>
-        </div>
-      </footer>
+    <footer class="footer">    
+  <div class="footer-left col-md-4 col-sm-6">
+    <p class="about">
+      <h1> About the company</h1> Ut congue augue non tellus bibendum, in varius tellus condimentum. In scelerisque nibh tortor, sed rhoncus odio condimentum in. Sed sed est ut sapien ultrices eleifend. Integer tellus est, vehicula eu lectus tincidunt,
+      ultricies feugiat leo. Suspendisse tellus elit, pharetra in hendrerit ut, aliquam quis augue. Nam ut nibh mollis, tristique ante sed, viverra massa.
+    </p>
+    <div class="icons">
+      <a href="https://www.facebook.com/"><i class="icon-facebook"></i></a>
+      <a href="https://www.twitter.com/"><i class="icon-twitter"></i></a>
+      <a href="https://www.linkedin.com/"><i class="icon-linkedin"></i></a>
+      <a href="https://www.github.com/"><i class="icon-github"></i></a>
+      <a href="https://www.instagram.com/"><i class="icon-instagram"></i></a>
     </div>
+  </div>
+  
+  <div class="footer-center col-md-4 col-sm-6">
+  
+    <div>
+      <i class="icon-map-marker"></i>
+      <p><span> Street name and number</span> City, Country</p>
+    </div>
+    <div>
+      <i class="icon-phone"></i>
+      <p> (+00) 0000 000 000</p>
+    </div>
+    <div>
+      <i class="icon-envelope"></i>
+      <p> Codeflow@company.com</p>
+    </div>
+  </div>
+  <div class="footer-right col-md-4 col-sm-6">
+    
+    <p class="menu">
+    <Link to="/"> Home</Link> <br></br> 
+    <Link to="/events"> Events</Link> <br></br>
+    <Link to="/Hackathons"> Hackathons</Link> <br></br>
+    <Link to="/"> Open Source</Link> <br></br>
+    <Link to="/"> Collaborations</Link> <br></br>
+    <Link to="/"> Terms and Conditions</Link> 
+    </p>
+    <p class="name"> Codeflow &copy; 2021</p>
+  </div>
+</footer>
   );
 };
 
